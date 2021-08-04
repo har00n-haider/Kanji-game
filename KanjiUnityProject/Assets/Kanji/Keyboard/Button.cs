ed4a1abcb296ef7723b553fcb99f5321a07236ec
haroon-haider <haroon.haider@3drepo.org>
2021-08-04 09:35:34 +0100

    adding a keyboard place holder to the project for kana/romaji input

---
+++ KanjiUnityProject/Assets/Kanji/Keyboard/Button.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class Button : MonoBehaviour
+{
+
+    TextMeshProUGUI textMesh;
+    [SerializeField]
+    private char buttonChar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textMesh = GetComponentInChildren<TextMeshProUGUI>();
+        textMesh.text = buttonChar.ToString();
+        gameObject.name = "Button " + textMesh.text;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
