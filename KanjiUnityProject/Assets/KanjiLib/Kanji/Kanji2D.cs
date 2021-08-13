ef74246ebe30959065db31b88281126f3388b815
haroon-haider <haroon.haider@3drepo.org>
2021-08-13 13:23:05 +0100

    Initial setup of the 2D kanji

--- KanjiUnityProject/Assets/KanjiLib/Kanji/Kanji2D.cs
+++ KanjiUnityProject/Assets/KanjiLib/Kanji/Kanji2D.cs
@@ -8,10 +8,25 @@ public class Kanji2D : Kanji
 {
     private KanjiGrid2D kanjiGrid;
 
+    public override void Init(KanjiData kanjiData)
+    {
+        base.Init(kanjiData);
+        // setup the grid
+        kanjiGrid = GetComponentInChildren<KanjiGrid2D>();
+        kanjiGrid.Init(parsedKanjiData);
+    }
 
     protected override void UpdateInput()
     {
-
+        if (Input.GetMouseButton(0))
+        {
+            curStroke.inpStroke.AddPoint(Input.mousePosition);
+        }
+        // clear line
+        if (Input.GetMouseButtonUp(0))
+        {
+            curStroke.inpStroke.Complete();
+        }
     }
 
 }
