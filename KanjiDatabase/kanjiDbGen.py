1ca71e0fb5224c741f62e62e9e7d9bfa4b9429fc
haroon-haider <haroon.haider@3drepo.org>
2021-08-23 08:16:46 +0100

    updating database generation code for katakana and hiragana entries

--- KanjiDatabase/kanjiDbGen.py
+++ KanjiDatabase/kanjiDbGen.py
@@ -1,91 +1,94 @@
 from lxml import etree
 from os import path
 
+def PopulatRootWithSet(root, inputSet, inputSetName, inCategoryType="basic"):
+
+  for setIdx, kanji in enumerate(inputSet):
+    kanjiCode = format(ord(kanji), "x")
+
+    # make sure the kanji entry file exists, otherwise bail
+    kanjiSvgPath = path.join(kanjiDir,  '0' + kanjiCode + '.svg')
+    if not path.exists(kanjiSvgPath):
+      continue
+
+    # make the element for this kanji
+    outputKanjiElem = etree.Element('kanji', code=kanjiCode)
+
+    # add the kanji character literal
+    literalElem = etree.Element('literal', code=kanjiCode)
+    literalElem.text = kanji
+    outputKanjiElem.insert(len(outputKanjiElem), literalElem)
+    
+    # attempt to establish dict entry for the required kanji 
+    dicKanjiElemMatches = dictionaryRoot.xpath('.//literal[text()="' + kanji + '"]')
+    dictEntryExists = len(dicKanjiElemMatches) > 0
+    if dictEntryExists:
+      dicKanjiElem = dicKanjiElemMatches[0].getparent()
+
+      # reading group (new)
+      outputReadingElem = etree.Element('reading_group')
+      readingElems = dicKanjiElem.xpath('.//reading[@r_type="ja_on"]') 
+      readingElems.extend(dicKanjiElem.xpath('.//reading[@r_type="ja_kun"]'))
+      for elem in readingElems:
+        outputReadingElem.insert(0, elem)
+      outputKanjiElem.insert(len(outputKanjiElem), outputReadingElem)
+
+      # meaning group (new)
+      outputMeaningElem = etree.Element('meaning_group')
+      meaningElems = dicKanjiElem.xpath('.//meaning[not(@*)]') # only english
+      for elem in meaningElems:
+        outputMeaningElem.insert(0, elem)
+      outputKanjiElem.insert(len(outputKanjiElem), outputMeaningElem)
+
+    # svg (new)
+    # Searching the kanji svg by path
+    outputSvgElem = etree.Element('svg')
+    kanjiSvgRoot = etree.parse(kanjiSvgPath, parser).getroot()
+    nsmap = {'xmlns' :kanjiSvgRoot.nsmap[None]}
+    reqGElement = kanjiSvgRoot.xpath('.//xmlns:g[@id="kvg:0' + kanjiCode + '"]', namespaces=nsmap)[0]
+    # Remove namespace prefixes from tags
+    for elem in reqGElement.getiterator():
+        elem.tag = etree.QName(elem).localname
+        # clean attributes
+        for a, v in elem.items():
+            q = etree.QName(a)
+            del elem.attrib[a]
+            elem.attrib[q.localname] = v
+    # Remove unused namespace declarations
+    etree.cleanup_namespaces(reqGElement)
+    outputSvgElem.insert(0, reqGElement)
+    # nsmap['kvg'] = reqGElement.nsmap['kvg']
+    # x = reqGElement.xpath('./@kvg:element', namespaces=nsmap)
+    outputKanjiElem.insert(len(outputKanjiElem), outputSvgElem)
+
+    # custom category (new)
+    outputCategoryElem = etree.Element('category', type=inCategoryType)
+    outputCategoryElem.text = str(inputSetName)
+    outputKanjiElem.insert(len(outputKanjiElem), outputCategoryElem)
+
+    # add to root
+    root.insert(setIdx, outputKanjiElem)
+
+    print("Generated entry for:" +  kanji + ", dict entry exists: " + str(dictEntryExists))
+  return
+
+# paths
 dictionaryPath = 'kanjidic2.xml\kanjidic2.xml'
 kanjiDir = 'kanji'
 outputPath = 'kanjiDb.xml'
 
-# Create the output root
-root = etree.Element('kanjidb')
-
-kanjiSetNo = 1
-kanjiSet = [
-  '一',
-  '九',
-  '七',
-  '十',
-  '人',
-  '二',
-  '入',
-  '八',
-  '下',
-  '口',
-  '三',
-  '山'
-]
-
+# top level requirements
+outRoot = etree.Element('kanjidb')
 parser = etree.XMLParser(remove_blank_text=True)
-
 dictionaryRoot = etree.parse(dictionaryPath, parser).getroot()
 
-for setIdx, kanji in enumerate(kanjiSet):
-
-  # establish root of the required kanji 
-  dicKanjiElem = dictionaryRoot.xpath('.//literal[text()="' + kanji + '"]')[0].getparent()
-  kanjiCode = dicKanjiElem.xpath('.//cp_value[@cp_type="ucs"]')[0].text
-  outputKanjiElem = etree.Element('kanji', code=kanjiCode)
-
-  # take kanji character literal
-  literalElem = dicKanjiElem.xpath('.//literal')[0]
-  outputKanjiElem.insert(len(outputKanjiElem), literalElem)
-  
-  # reading group (new)
-  outputReadingElem = etree.Element('reading_group')
-  readingElems = dicKanjiElem.xpath('.//reading[@r_type="ja_on"]') 
-  readingElems.extend(dicKanjiElem.xpath('.//reading[@r_type="ja_kun"]'))
-  for elem in readingElems:
-    outputReadingElem.insert(0, elem)
-  outputKanjiElem.insert(len(outputKanjiElem), outputReadingElem)
-
-  # meaning group (new)
-  outputMeaningElem = etree.Element('meaning_group')
-  meaningElems = dicKanjiElem.xpath('.//meaning[not(@*)]') # only english
-  for elem in meaningElems:
-    outputMeaningElem.insert(0, elem)
-  outputKanjiElem.insert(len(outputKanjiElem), outputMeaningElem)
-
-  # svg (new)
-  # Searching the kanji svg by path
-  outputSvgElem = etree.Element('svg')
-  kanjiSvgPath = path.join(kanjiDir,  '0' + kanjiCode + '.svg')
-  kanjiSvgRoot = etree.parse(kanjiSvgPath, parser).getroot()
-  nsmap = {'xmlns' :kanjiSvgRoot.nsmap[None]}
-  reqGElement = kanjiSvgRoot.xpath('.//xmlns:g[@id="kvg:0' + kanjiCode + '"]', namespaces=nsmap)[0]
-  # Remove namespace prefixes from tags
-  for elem in reqGElement.getiterator():
-      elem.tag = etree.QName(elem).localname
-      # clean attributes
-      for a, v in elem.items():
-          q = etree.QName(a)
-          del elem.attrib[a]
-          elem.attrib[q.localname] = v
-  # Remove unused namespace declarations
-  etree.cleanup_namespaces(reqGElement)
-  outputSvgElem.insert(0, reqGElement)
-  # nsmap['kvg'] = reqGElement.nsmap['kvg']
-  # x = reqGElement.xpath('./@kvg:element', namespaces=nsmap)
-  outputKanjiElem.insert(len(outputKanjiElem), outputSvgElem)
-
-  # custom category (new)
-  outputCategoryElem = etree.Element('category', type="kanjipower")
-  outputCategoryElem.text = str(kanjiSetNo)
-  outputKanjiElem.insert(len(outputKanjiElem), outputCategoryElem)
-
-  # add to root
-  root.insert(setIdx, outputKanjiElem)
+# kanji sets
+PopulatRootWithSet(outRoot, ['一','九','七','十','人','二','入','八','下','口','三','山'], "kanji power set 1")
+PopulatRootWithSet(outRoot, [chr(i) for i in range(0x30A0, 0x30FF)] , "katakana set")
+PopulatRootWithSet(outRoot, [chr(i) for i in range(0x3040, 0x309F)] , "hiragana set")
 
 with open('kanjigamedb.xml', 'wb') as doc:
-  doc.write(etree.tostring(root, pretty_print = False, xml_declaration=True, encoding='UTF-8'))
+  doc.write(etree.tostring(outRoot, pretty_print = True, xml_declaration=True, encoding='UTF-8'))
 
 
 
