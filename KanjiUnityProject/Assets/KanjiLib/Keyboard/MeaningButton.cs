0f30872d05df642cba77e59837f436664cb26278
haroon-haider <haroon.haider@3drepo.org>
2021-09-14 11:37:23 +0100

    adding scripts for the meaning input

---
+++ KanjiUnityProject/Assets/KanjiLib/Keyboard/MeaningButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MeaningButton : MonoBehaviour
+{
+    [HideInInspector]
+    public MeaningInput meaningInput;
+    private TextMeshProUGUI textMesh;
+    public string text { get { return textMesh.text; } set { textMesh.text = value; } }
+    private Button button;
+
+    public void Init()
+    {
+        button = GetComponent<Button>();
+        textMesh = GetComponentInChildren<TextMeshProUGUI>();
+        button.onClick.AddListener(() => meaningInput.UpdateWordMeaning(textMesh.text));
+    }
+
+    private void OnDestroy()
+    {
+        button.onClick.RemoveAllListeners();
+    }
+
+}
---
+++ KanjiUnityProject/Assets/KanjiLib/Keyboard/MeaningInput.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MeaningInput : MonoBehaviour
+{
+    // configuration
+    public int noOfButtons = 3;
+    
+    // state
+    private int correctMeaningIdx;
+
+    // refs
+    [SerializeField]
+    private GameObject meaningButtonPrefab;
+    private MeaningButton[] meaningButtons;
+    [HideInInspector]
+    public Keyboard keyboard;
+
+    void Start()
+    {
+        meaningButtons = new MeaningButton[noOfButtons];
+        for (int i = 0; i < noOfButtons; i++)
+        {
+            MeaningButton button = Instantiate(meaningButtonPrefab, transform).GetComponent<MeaningButton>();
+            button.meaningInput = this;
+            button.Init();
+            meaningButtons[i] = button;
+        }
+    }
+
+    public void SetPromptWord(PromptWord promptWord) 
+    {
+        List<string> fillerMeanings = keyboard.kanjiMan.GetRandomMeanings(
+            noOfButtons, 
+            promptWord.meanings[0]);
+        correctMeaningIdx = Random.Range(0, noOfButtons);
+        for (int i = 0; i < meaningButtons.Length; i++)
+        {
+            meaningButtons[i].text = i == correctMeaningIdx ? 
+                promptWord.meanings[0] : 
+                fillerMeanings[i];
+        }
+    }
+
+    public void UpdateWordMeaning(string wordMeaning) 
+    {
+        if(meaningButtons[correctMeaningIdx].text == wordMeaning) 
+        {
+            keyboard.WordCompleted();
+        }
+    }         
+
+}
