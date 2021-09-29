9882e71bf207f154b10a60fcd73d57a91dfb7ddd
haroon-haider <haroon.haider@3drepo.org>
2021-09-29 11:01:07 +0100

    added a camera controller script

---
+++ KanjiUnityProject/Assets/Camera/MainCamera.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+    
+public class MainCamera : MonoBehaviour
+{
+    // refs
+
+    public GameObject mainCharacter;
+
+    [Range(0, 100)]
+    public float distanceToCharacter = 100f;
+
+    public float pitch;
+    public float yaw;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.rotation = Quaternion.Euler(pitch, yaw, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = 
+            mainCharacter.transform.position + -transform.forward * distanceToCharacter;
+    }
+}
